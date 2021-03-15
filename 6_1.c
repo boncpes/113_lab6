ed4b4d9a7c3f6aea77a0416c68f96e040e95204d
boncpes <78350722+boncpes@users.noreply.github.com>
2021-03-15 16:51:12 +0700

    final

--- 6_1.c
+++ 6_1.c
@@ -3,24 +3,26 @@ int main()
 {
     int n,i,j;
     scanf("%d",&n);
-    char w[n][1000],w[1000];
+    char b[n][1000],w[1000];
     for(i=0;i<n;i++)
     {
-        scanf(" %1000[^\n]s",n[i]);
+        scanf(" %1000[^\n]s",b[i]);
     }
     for(i=0;i<n-1;i++)
     {
         for(j=i+1;j<n;j++)
         {
-            if(strcmp(n[i],n[j])>0)
+            if(strcmp(b[i],b[j])>0)
             {
-                strcpy(w,n[i]);
-                strcpy(n[i],n[j]);
-                strcpy(n[j],w);
+                strcpy(w,b[i]);
+                strcpy(b[i],b[j]);
+                strcpy(b[j],w);
             }
         }
     }
-
-
+    for(i=0;i<n;i++)
+    {
+        printf("%s\n",b[i]);
+    }
     return 0;
 }
