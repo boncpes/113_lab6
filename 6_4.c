2ceeed6241b85cc5836744f2b62cf475dbda12f9
boncpes <78350722+boncpes@users.noreply.github.com>
2021-03-15 18:43:21 +0700

    6_4 change if in loop

--- 6_4.c
+++ 6_4.c
@@ -3,13 +3,15 @@ int main()
 {
     int n,i,a,b,max=0,min=0,p;
     scanf("%d",&n);
-    int student[n];
+    int student[n],sum[2];
     for(i=0; i<n; i++)
     {
         student[i]=0;
     }
     for(i=0; i<n; i++)
     {
+        a=0;
+        b=0;
         scanf("%d %d",&a,&b);
         student[a-1]+=1;
         student[b-1]+=1;
@@ -18,11 +20,11 @@ int main()
     min=student[0];
     for(i=1; i<n; i++)
     {
-        if(student[i]>max)
+        if(student[i]>max && student[i]!=0)
         {
             max=student[i];
         }
-        if(min>student[i])
+        if(min>student[i]&& student[i]!=0)
         {
             min=student[i];
         }
@@ -31,7 +33,7 @@ int main()
     {
         if(max==student[i])
         {
-            printf("%d ",i+1);
+            sum[0]=i;
             break;
         }
     }
@@ -39,9 +41,21 @@ int main()
     {
         if(min==student[i])
         {
-            printf("%d",i+1);
+            sum[1]=i;
             break;
         }
     }
+    if(sum[0]==sum[1])
+    {
+        for(i=0; i<n; i++)
+        {
+            if(min==student[i] && sum[1]!=i)
+            {
+                sum[1]=i;
+                break;
+            }
+        }
+    }
+    printf("%d %d",sum[0]+1,sum[1]+1);
     return 0;
 }
